d36a2837c058460deee462b3dc673dd44040c102
Billal070 <billalhossain98609@gmail.com>
2026-06-30 17:13:55 +0600

    Update Supabase URL and Publishable Key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // সুপাবেস প্রজেক্টের URL এবং Publishable (anon) Key
-const supabaseUrl = 'YOUR_PROJECT_URL'; // এখানে আপনার আসল URL বসান
-const supabaseAnonKey = 'YOUR_PUBLISHABLE_KEY'; // এখানে আপনার Publishable Key বসান
+const supabaseUrl = 'https://ttqkfumqwvkbiuaszwra.supabase.co'; // এখানে আপনার আসল URL বসান
+const supabaseAnonKey = 'sb_publishable_oA033hMNLc97jfinWvLgAA_aXNM_DMT'; // এখানে আপনার Publishable Key বসান
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
