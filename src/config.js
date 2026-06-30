f7e8f5a50458a4cab7c4acdd6a60a1d5c816468c
Billal070 <billalhossain98609@gmail.com>
2026-06-30 20:33:57 +0600

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  siteName: "Cash x BD",
+  logoUrl: "https://i.postimg.cc/vZj3whQL/earnova.jpg", // উদাহরণ: "https://i.postimg.cc/xxxxxx/logo.png"
+  telegramLink: "https://t.me/your_channel_username", // আপনার টেলিগ্রাম গ্রুপ বা চ্যানেলের লিংক
+  adsterraLink: "https://www.example.com", // আপনার অ্যাডস্টেরা ডিরেক্ট লিংক (আপাতত উদাহরণ দেওয়া)
+  
+  // আয়ের অন্যান্য কনফিগারেশন (ভবিষ্যতে সহজে পরিবর্তনের জন্য)
+  activationFee: 150,
+  perAdReward: 5,
+  referralBonus: 30,
+  minWithdrawFirst: 75,
+  minWithdrawSubsequent: 200,
+};
